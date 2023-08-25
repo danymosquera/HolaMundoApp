379961d74b73c408ebb35097bb9d7ba4326c224b
dmosquera <139640744+dmosquera@cfa.com.co>
2023-08-25 00:56:16 -0500

    AvanceJuyevesNoche
    AvanceJuyevesNoche

--- HolaMundoApp/HolaMundoApp/Settings.cs
+++ HolaMundoApp/HolaMundoApp/Settings.cs
@@ -6,7 +6,7 @@ namespace HolaMundoApp
 {
     class Settings
     {
-        //public const string ApiBaseUri = "http://192.168.1.3:5000/api";
-        public const string ApiBaseUri = "http://10.101.1.162:5000/api";
+        public const string ApiBaseUri = "http://192.168.1.3:5000/api";
+        //public const string ApiBaseUri = "http://10.101.1.162:5000/api";
     }
 }
